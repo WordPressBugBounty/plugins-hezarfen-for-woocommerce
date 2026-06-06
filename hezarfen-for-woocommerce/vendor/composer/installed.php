90608576b1f8575004e3bae5918dfa8c6a71c866
Sean Murphy <sean@iamseanmurphy.com>
2026-06-06 14:46:19 +0000

    2.12.0

--- hezarfen-for-woocommerce/vendor/composer/installed.php
+++ hezarfen-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '2.11.5',
-        'version' => '2.11.5.0',
-        'reference' => '82c5b007fd8991d2947d9b92780a3fafd6938545',
+        'pretty_version' => '2.12.0',
+        'version' => '2.12.0.0',
+        'reference' => '4e2229f0354611efb01b3fe2a0a2e773aa6fca23',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '2.11.5',
-            'version' => '2.11.5.0',
-            'reference' => '82c5b007fd8991d2947d9b92780a3fafd6938545',
+            'pretty_version' => '2.12.0',
+            'version' => '2.12.0.0',
+            'reference' => '4e2229f0354611efb01b3fe2a0a2e773aa6fca23',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
